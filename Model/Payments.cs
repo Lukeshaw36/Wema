fd9b0a15aa79a31c384040908ac7a4e4b90d056c
Lukeshaw36 <103990852+Lukeshaw36@users.noreply.github.com>
2023-11-22 08:43:12 +0100

    Add project files.

---
+++ Model/Payments.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wema.Bit.Model
+{
+    public class Payments
+    {
+        public int PaymentId { get; set; }
+        public int UserId { get; set; } 
+        public int PayAmount { get; set; }    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.Transactions;
+using Wema.Bit;
+using Wema.Bit.Model;
+
+namespace Wema.Bit;
+
+//    public class User
+//    {
+//        public static void Main(string[] args)
+
+//        {
+//            Console.WriteLine("Hello world!");
+
+//            Payment.Greet();
+//            Payment p = new Payment();
+//            p.GreetMe();
+
+//            string greet = p.GreetMe();
+
+//            Console.WriteLine(greet);
+
+//        }
+//    }
+
+//    public class Payment
+//    {
+//        public static void Greet()
+//        {
+//            Console.WriteLine("Good Afternoon");
+//        }
+
+//        public string GreetMe()
+//        {
+//            Console.WriteLine("Good Afternoon");
+
+//            return "Good Afternoon ma";
+//        }
+//    }
+//}
+
+
+
+public class User
+{
+    public static void Main(string[] args)
+
+    {
+
+        List<Users> users = new List<Users>()
+        {
+             new Users() {Id = 1, FirstName="Luke", LastName="Duvbiama",
+            Email="Lukemorlakemi@gmail.com"},
+            new Users() {Id = 2, FirstName="James", LastName="Adebayo",
+            Email="jamesadebayo@gmail.com"},
+            new Users() {Id= 3, FirstName="Sam", LastName="Jdoka",
+            Email="Samjohn@gmail.com"},
+            new Users() {Id = 4, FirstName="Cynthia", LastName="Morgan",
+            Email="josephudokae@gmail.com"},
+            new Users() {Id = 5, FirstName="Micheal", LastName="Jackson",
+            Email="Michealjackson@gmail.com"},
+        };
+
+        List<Payments> payments = new List<Payments>()
+        {
+
+            new Payments() {UserId = 1, PaymentId = 1, PayAmount = 2500},
+            new Payments() {UserId = 2, PaymentId = 2, PayAmount = 1400},
+            new Payments() {UserId = 3, PaymentId = 3, PayAmount = 2600},
+            new Payments() {UserId = 4, PaymentId = 4, PayAmount = 6700},
+            new Payments() {UserId = 5, PaymentId = 5, PayAmount = 8900},
+        };
+
+
+        foreach (var user in users)
+        {
+            Console.WriteLine($"User: {user.FirstName} {user.LastName}, Email: {user.Email}");
+
+
+            var userPayments = payments.Where(p => p.UserId == user.Id);
+
+
+            foreach (var userPayment in userPayments)
+            {
+                Console.WriteLine($"  PaymentId: {userPayment.PaymentId}, Amount: {userPayment.PayAmount}");
+            }
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
